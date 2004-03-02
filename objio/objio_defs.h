12001f15883f0ccdba85a46a58bd56d97199d01a
Nicolas Regnault <nicolas.regnault@lpnhe.in2p3.fr>
2004-03-02 18:46:04 +0000

    First version.

---
+++ objio/objio_defs.h
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+// $Id: objio_defs.h,v 1.1 2004/03/02 18:46:04 nrl Exp $
+// 
+// \file objio_defs.h
+// 
+// 
+#ifndef OBJIO_DEFS_H
+#define OBJIO_DEFS_H
+
+#define OBJIO_READ  0
+#define OBJIO_WRITE 1
+
+
+
+#endif
+
