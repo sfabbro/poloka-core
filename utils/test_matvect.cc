5146a666eaaec05bd224ccbe6bc03ee316b8a24f
Julien Guy <guy@lpnp204.in2p3.fr>
2004-05-08 13:57:28 +0000

    more test

--- utils/test_matvect.cc
+++ utils/test_matvect.cc
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-static void usage(const char *pgname)
-{
-  cerr << pgname << " " << endl ;
-}
 
 int main(int argc, char **argv)
 {
@@ -28,6 +24,29 @@ int main(int argc, char **argv)
   cout << m1 << endl;
   cout << m2 << endl;
   cout << m3 << endl;
+  
+  Vect v1(3);
+  v1(0)=1;
+  v1(1)=2;
+  v1(2)=3;
+
+  cout << v1 << endl;
+  Vect v2 = v1*2.;
+  cout << v2 << endl;
+  cout << v1*v1 << endl;
+
+  Mat m4 = v1.asMat();
+  cout << m4 << endl;
+  
+  Mat m5(3,3);
+  m5.Identity();
+  Vect toto = m5*v1;
+  cout << m5*v1 << endl;
+  cout << toto << endl;
+  cout << "==========" << endl;
+  cout << double(v1.transposed()*m5*v1) << endl;
+  cout << double(v1.transposed()*v1) << endl;
+  cout << v1.asMat()*v1.transposed() << endl;
   
   return 0;
 }
