9adf0cc65d37d4fcc385555661e25ef84f405807
Julien Guy <guy@lpnp204.in2p3.fr>
2004-05-14 15:37:03 +0000

    it does it

--- utils/fitnight.cc
+++ utils/fitnight.cc
@@ -2,12 +2,20 @@
 #include <fstream>
 #include <matvect.h>
 
+#include "lightcurvepoint.h"
+#include "lightcurvepoint_dict.h"
+#include "objio.h"
+#include "typemgr.h"
+
 using namespace std;
+
+
 
 
 int main(int argc, char **argv)
 {
   
+  // vecteur de flux
   Vect FluxVec;
   {
     Mat m;
@@ -16,37 +24,79 @@ int main(int argc, char **argv)
     FluxVec = m;
   }
   int nflux = FluxVec.Size();
-
   
+  
+  // matrice de covariance
   Mat CovarianceMat;  
   if(CovarianceMat.readFits("pmat_sn.fits")!=0)
     return -1;
   Mat FluxCovarianceMat = CovarianceMat.SubBlock(0,nflux-1,0,nflux-1);
   FluxCovarianceMat.Symmetrize("L");
-  Mat FluxWeightMat = FluxCovarianceMat;
-  FluxWeightMat.SymMatInvert();
-  // test
-  //Mat res = FluxWeightMat*FluxCovarianceMat;
-  //res.writeFits("toto.fits");
-  //cout << res.SizeX() << " " << res.SizeY() << endl;
-  
- 
   Mat A;
   A.readFits("nightmat_sn.fits");
   cout << A << endl;
   
-  cout << FluxWeightMat.SizeX() << " " << FluxWeightMat.SizeY() << endl;
-  cout << A.SizeX() << " " << A.SizeY() << endl;
-  Mat AtWA = A.transposed()*FluxWeightMat*A;
-  cout << AtWA << endl;
-  Mat AtWA_invert = AtWA; AtWA_invert.SymMatInvert();
+  vector<int> suppressedfluxes;
+  while(true) {
   
-  Vect flux_per_night = (AtWA_invert*(A.transposed()*FluxWeightMat))*FluxVec;
-  cout << "Mean flux per night" << endl;
-  cout << flux_per_night << endl;
-  cout << "Covariance matrix" << endl;
-  cout << AtWA_invert << endl;
+    Mat FluxWeightMat = FluxCovarianceMat;
+    FluxWeightMat.SymMatInvert();
+    Mat AtWA = A.transposed()*FluxWeightMat*A;
+    Mat AtWA_invert = AtWA; AtWA_invert.SymMatInvert();
+    Vect flux_per_night = (AtWA_invert*(A.transposed()*FluxWeightMat))*FluxVec;
+    cout << "Mean flux per night" << endl;
+    cout << flux_per_night << endl;
+    cout << "Covariance matrix" << endl;
+    cout << AtWA_invert << endl;
   
+    // now compute chi2
+    Vect B = FluxVec - A*flux_per_night;
+    double chi2 = B.transposed()*FluxWeightMat*B;
+    cout << "chi2 = " << chi2 << endl;
+    
+    int ndf = A.SizeY()-A.SizeX();
+    cout << "ndf = " << ndf << endl;
+    cout << "chi2/ndf = " << chi2/ndf << endl;
+    
+    if(chi2/ndf<1.5 || suppressedfluxes.size()>=6)
+      break;
+
+    int outlier = -1;
+    double flux_chi2;
+    double chi2_max = 0;
+    for(unsigned int iflux = 0 ;iflux<B.Size();++iflux) {
+      
+      flux_chi2 = pow(B(iflux),2)*FluxWeightMat(iflux,iflux);
+      if(flux_chi2>chi2_max) {
+	chi2_max = flux_chi2;
+	outlier = iflux;
+      }
+    }
+    
+    cout << outlier << " " << sqrt(chi2_max) << endl;
+    
+    if(sqrt(chi2_max)<3.)
+      break;
+    suppressedfluxes.push_back(outlier);
+    // on vire cet outlier de toutes les matrices
+    A = A.WithoutRows(outlier,outlier);
+    {
+      Mat mFluxVec = FluxVec;
+      FluxVec = mFluxVec.WithoutRows(outlier,outlier);
+      //cout << FluxVec << endl;
+    }
+    FluxCovarianceMat = FluxCovarianceMat.WithoutRows(outlier,outlier);
+    FluxCovarianceMat = FluxCovarianceMat.WithoutColumns(outlier,outlier);
+  }
+  return 0;
+
+  //list< CountedRef<LightCurvePoint> > lcpoints;
+  //obj_input<xmlstream> oi("lc.xml");
+  //oi >> lcpoints;
+  //oi.close();
+  //cout << lcpoints.size() << endl;
+  
+
   return 0;
 }
 
