47d6b4b1a30346cc7e5db7d1861a116a7d47ec16
Julien Guy <guy@lpnp204.in2p3.fr>
2004-06-21 13:40:45 +0000

    add modelvignet

---
+++ utils/modelvignet.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <lightcurve.h>
+#include <simfitphot.h>
+#include <dimage.h>
+
+using namespace std;
+
+static void usage(const char *pgname)
+{
+  cerr << pgname << " <filename> " << endl ;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc < 2)  {usage(argv[0]);  exit(1);}
+
+ 
+
+  ifstream lightfile(argv[1]);
+  if (!lightfile) return EXIT_FAILURE;
+  
+  LightCurveList fids(lightfile);
+  LightCurve &lc = *(fids.begin());
+  int radius = 20;
+  
+  PhotStar* star = lc.Ref;
+  SimFitRefVignet *vignet = new SimFitRefVignet(star,lc.Ref->Image(),radius);
+  
+  Kernel galaxy;
+  string galname = lc.Ref->name+"/galaxy_sn.fits";
+  if(!FileExists(galname)) {
+    cout << " cannot open " << galname << endl;
+    return -12;
+  }
+  
+  galaxy.readFits(galname);
+  int hx = galaxy.HSizeX();
+  int hy = galaxy.HSizeY();
+  
+
+  // get sn position and max flux
+  float x,y,fluxmax;
+  {
+    string lcname = lc.Ref->name+"/lightcurve_sn.dat";
+    if(!FileExists(lcname)) {
+      cout << " cannot open " << lcname << endl;
+      return -12;
+    }
+    ifstream lcfile(lcname.c_str());
+    string line;
+    float flux;
+    fluxmax = -12;
+    while(getline(lcfile,line)) {
+      if(line[0]=='#')
+	continue;
+      sscanf(line.c_str(),"x %f y %f flux %f",&x,&y,&flux);
+      if(flux>fluxmax)
+	fluxmax=flux;
+    }
+    lcfile.close();
+  }
+  
+  cout << "x y fluxmax = " << x << " " << y << " " << fluxmax << endl;
+  
+  // update PSF
+  vignet->Psf.Tabulate(Point(x,y),*(vignet->psf),(const Window&)*vignet);
+    
+  // write image
+  Kernel model = galaxy;
+  for(int j=-hy;j<=hy;++j) {
+      for(int i=-hx;i<=hx;++i) {
+	model(i,j)+=fluxmax*vignet->Psf(i,j);
+      }
+  }
+  model.writeFits("model.fits");
+  
+
+  return EXIT_SUCCESS;
+}
+
