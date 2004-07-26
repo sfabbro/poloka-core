ff936552cc0ae452ed0c697b7fac28a18aa7b784
Delphine Hardin <hardin@lpnp204.in2p3.fr>
2004-07-26 18:28:15 +0000

    une image que l'on cree a partir d'une reducedimage sur laquelle on colle des supernovae

---
+++ mc/mcimage.h
@@ -0,0 +1,37 @@
+// This may look like C code, but it is really -*- C++ -*-
+#ifndef MCIMAGE__H
+#define MCIMAGE__H 
+
+#include "gtransfo.h"
+#include "basestar.h"
+#include "reducedimage.h"
+
+typedef enum AddMethod{WModel =0, WGaussian, WDaoPsf};
+
+
+class SimSNStarList;
+
+class MCImage : public ReducedImage 
+{
+  ReducedImageRef Source ;
+  GtransfoRef Transfo_fromref ;
+
+  SimSNStarList* SNList;
+
+  AddMethod Methode ;
+  
+SimSNStarList*  PrepareList();
+public :
+  MCImage(string name, const ReducedImageRef source, const GtransfoRef Transfo, SimSNStarList* List, AddMethod a_method  ); 
+  MCImage(string name,const ReducedImageRef source, SimSNStarList* List, AddMethod a_method  );
+  MCImage(string name, const ReducedImageRef Ref, const ReducedImageRef source, SimSNStarList* List, AddMethod a_method  );
+
+bool MakeFits();
+bool MakeCatalog();
+bool MakeWeight() ;
+bool MakeBad() ;
+bool MakeSatur(); 
+};
+
+
+#endif
