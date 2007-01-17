1e39176804b595addb498d07b6b0558f2cd7a0ca
Pierre Astier <pierre.astier@in2p3.fr>
2007-01-17 09:56:10 +0000

    now exceptions aware

--- utils/skylev.cc
+++ utils/skylev.cc
@@ -5,6 +5,8 @@
 #include "frame.h"
 #include "fitstoad.h"
 #include "alltelinst.h"
+#include "polokaexception.h"
+
 
 static void usage(const char *prog)
 {
@@ -17,6 +19,7 @@ static void usage(const char *prog)
 int main(int argc,char **args)
 {
   bool splitAmps = false;
+  bool ok = true;
   if (argc <=1)
     {
       usage(args[0]);
@@ -33,48 +36,51 @@ int main(int argc,char **args)
 	    }
 	  continue;
 	}
-      FitsImage image(arg);
-      if (!image.IsValid())
+      try
 	{
-	  cerr << " skylev : invalid file : "  << args[i] << endl;
-	  continue;
-	}
-      int Namp;
-      if (splitAmps && image.HasKey("TOADNAMP")) Namp = image.KeyVal("TOADNAMP");
-      else Namp = 1;
+	  FitsImage image(arg);
+	  int Namp;
+	  if (splitAmps && image.HasKey("TOADNAMP")) Namp = image.KeyVal("TOADNAMP");
+	  else Namp = 1;
       
-      Pixel mean,sigma;
-      if (Namp==1)
-      {
-	FitsHeader  &head = image;
-	Pixel meanOS = 0, sigmaOS =0;
-	if (!head.HasKey("WRITEDAT"))
-	  {
-	    Frame overscan= OverscanRegion(head,Namp);
-	    if (overscan.Area() != 0)
-	      {
-		cout << " raw image: subtract bias from the mean" << endl;
-		meanOS = image.MedianInFrame(overscan, sigmaOS);
-		cout << "Median of overscan region " << meanOS << endl;
-	      }
-	  }
-	image.SkyLevel((head),& mean, &sigma);
-	cout << args[i] << ' ' << "  m : " << mean - meanOS<< " s : " << sigma << endl;
-      }
-    else
-      {
-	cout << args[i] << " : " << endl;
-	Frame TotalIllu = TotalIlluRegion(image);
-	for (int k=1; k<= Namp; ++k)
-	  {
-	    Frame region;
-	    if (image.Nx() == TotalIllu.Nx() && image.Ny() == TotalIllu.Ny()) region = AmpRegion(image,k);
-	    else region = IlluRegion(image,k);
-	    image.SkyLevel(region,&mean,&sigma);
-	    cout << " ampli("<< k  <<")  m : " << mean << " s : " << sigma << endl;
-	  }
-      }    
-    }
-  return EXIT_SUCCESS;
+	  Pixel mean,sigma;
+	  if (Namp==1)
+	    {
+	      FitsHeader  &head = image;
+	      Pixel meanOS = 0, sigmaOS =0;
+	      if (!head.HasKey("WRITEDAT"))
+		{
+		  Frame overscan= OverscanRegion(head,Namp);
+		  if (overscan.Area() != 0)
+		    {
+		      cout << " raw image: subtract bias from the mean" << endl;
+		      meanOS = image.MedianInFrame(overscan, sigmaOS);
+		      cout << "Median of overscan region " << meanOS << endl;
+		    }
+		}
+	      image.SkyLevel((head),& mean, &sigma);
+	      cout << args[i] << ' ' << "  m : " << mean - meanOS<< " s : " << sigma << endl;
+	    }
+	  else // several amps
+	    {
+	      cout << args[i] << " : " << endl;
+	      Frame TotalIllu = TotalIlluRegion(image);
+	      for (int k=1; k<= Namp; ++k)
+		{
+		  Frame region;
+		  if (image.Nx() == TotalIllu.Nx() && image.Ny() == TotalIllu.Ny()) region = AmpRegion(image,k);
+		  else region = IlluRegion(image,k);
+		  image.SkyLevel(region,&mean,&sigma);
+		  cout << " ampli("<< k  <<")  m : " << mean << " s : " << sigma << endl;
+		}
+	    }
+	}
+      catch(PolokaException p)
+	{
+	  p.PrintMessage(cout);
+	  ok = false;	      
+	} 
+    }// end of loop on args
+  return ((ok == true) ? EXIT_SUCCESS : EXIT_FAILURE);
 
 }
