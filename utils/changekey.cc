a1d87020a4e47d14b0003f2617baf0bf767adf2f
Pierre Astier <pierre.astier@in2p3.fr>
2007-08-03 16:08:43 +0000

    use typed routines rather than string for everybody. BZERO as a string messes up cfitio

--- utils/changekey.cc
+++ utils/changekey.cc
@@ -11,9 +11,34 @@ int main(int argc, char**argv)
       char *key = argv[2];
       const char *comment = "Changed key";
       FitsHeader toChange(in_name,RW);
-      if (argc==5) comment = argv[4];
-      cout << "Changing key " << key << " from " << toChange.KeyVal(key) << " to " << argv[3] << endl;
-      toChange.AddOrModKey(key,argv[3]);
+      if (argc==5) comment = argv[4]; // ?
+      string type;
+      char * endptr;
+      double dval = strtod(argv[3],&endptr);
+      string oldval(toChange.HasKey(key) ? string(toChange.KeyVal(key)) :  "<was absent>");
+      if (endptr != argv[3]) // means successful conversion to double
+	{
+	  type = "double";
+	  toChange.AddOrModKey(key,dval);
+	}
+      else if (strcmp(argv[3],"T") == 0)
+	{
+	  type = "bool ";
+          toChange.AddOrModKey(key,true);
+	}
+      else if (strcmp(argv[3],"F") == 0)
+	{
+	  type = "bool";
+          toChange.AddOrModKey(key,false);
+	}
+      else 
+	{
+	  type = "string";
+          toChange.AddOrModKey(key,string(argv[3]));
+	}
+      cout << "Changing key " << key << " from " << oldval
+	   << " to " << argv[3] << " (type=" << type << ')' <<  endl;
+
     }
   else 
     {
