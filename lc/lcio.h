eb093d735d7505289d8781bd10280ae5fd1aaaf5
Pierre Astier <pierre.astier@in2p3.fr>
2008-02-28 14:50:07 +0000

    almost nothing

--- lc/lcio.h
+++ lc/lcio.h
@@ -11,34 +11,34 @@ istream& lc_read(istream& Stream, RefStarList& Objects, ReducedImageList& Images
 //! create a light curve file from a list of objects and a ReducedImageList
 ostream& lc_write(ostream& Stream, const RefStarList& Objects, const ReducedImageList& Images);
 
-//!
-//! \page lightfile Syntax of the "lightfile"
-//! 
-//! A light curve file consists of objects and images.
-//! Comment are coded with the '#' at the beginning of the line.
-//! The expected format is the following:
-//! 
-//! \code
-//! 
-//! # X Y [DATE_MIN=DD/MM/YYYY DATE_MAX=DD/MM/YYYY NAME=toto ]
-//! OBJECTS
-//! 510.20 1043.98 DATE_MIN=10/08/2003 DATE_MAX=12/12/2003 NAME=R4D14-4
-//! 
-//! # The list of all DbImages in a single filter
-//! IMAGES
-//! DbImage1_R
-//! DbImage2_R
-//! DbImage1_I
-//! DbImage2_I
-//!   .
-//!   .
-//!   .
-//! # Optional photometric reference. The default is the best seeing image.
-//! PHOREF
-//! PhoRefDbImage
-//! 
-//! \endcode
-//! 
+/*!
+ \page lightfile Syntax of the "lightfile"
+ 
+ A light curve file consists of objects and images.
+ Comment are coded with the '#' at the beginning of the line.
+ The expected format is the following:
+ 
+ \code
+ 
+ # X Y [DATE_MIN=DD/MM/YYYY DATE_MAX=DD/MM/YYYY NAME=toto ]
+ OBJECTS
+ 510.20 1043.98 DATE_MIN=10/08/2003 DATE_MAX=12/12/2003 NAME=R4D14-4
+ 
+ # The list of all DbImages in a single filter
+ IMAGES
+ DbImage1_R
+ DbImage2_R
+ DbImage1_I
+ DbImage2_I
+   .
+   .
+   .
+ # Optional photometric reference. The default is the best seeing image.
+ PHOREF
+ PhoRefDbImage
+ 
+ \endcode
+*/ 
 
 
 #endif // LCUTILS__H
