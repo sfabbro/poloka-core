4ebf2f33cf5d216c24a018c6d4a82a9cb9b46105
Pierre Astier <pierre.astier@in2p3.fr>
2008-09-04 09:03:58 +0000

    bring it back to like even without MC

--- utils/makesub.cc
+++ utils/makesub.cc
@@ -1,8 +1,9 @@
 #include<iostream>
 
 #include "sub.h"
+#ifdef ENABLE_MC
 #include "mcsub.h"
- 
+#endif 
  
 static void usage(const char * prog)
 {
@@ -10,7 +11,11 @@ static void usage(const char * prog)
   cerr << prog << " [-o] [-S subfile] [-M]" << endl
        << "        -o : overwrite existing images (default = false)" << endl
        << "        -S subfile : to provide a subfile name (default = \"subfile\")" 
+#ifdef ENABLE_MC
        << "        -M : MC mode for efficiency computation" << endl;
+#else
+       << "        -M : MC mode for efficiency computation (disabled at compilation)" << endl;
+#endif
   exit(1);
 }
 
@@ -18,7 +23,9 @@ int main(int argc, char **argv)
 {
   string subfile = "subfile";
   bool overwrite = false;
+#ifdef ENABLE_MC
   bool MCmode = false ;
+#endif
   for (int i=1; i< argc; i++)
     {
       char *arg = argv[i];
@@ -27,7 +34,9 @@ int main(int argc, char **argv)
 	{
 	case 'h' : usage(argv[0]);
 	case 'o' : overwrite = true;break;
+#ifdef ENABLE_MC
 	case 'M' : MCmode = true;break;
+#endif
 	case 'S' : i++; if (i >= argc) usage(argv[0]);
 	  subfile = argv[i]; break;
 	default : cerr << " do not understand " << arg << endl; usage(argv[0]);
@@ -40,7 +49,9 @@ int main(int argc, char **argv)
     {
       Sub sub(subfile, overwrite);
       sub.DoIt();
+#ifdef ENABLE_MC
       if (MCmode)
 	MCProcess(sub);
+#endif
     }
 } 
