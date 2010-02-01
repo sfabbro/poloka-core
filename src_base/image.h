6fc6b87fa493a2c060052854006c7e5dc7ef053a
Pierre Astier <pierre.astier@in2p3.fr>
2010-02-01 14:59:46 +0000

    image.cc image.h :           - moved ImageWindow and PixelIterator form header to cc file.           - added a ToDo section in cc file           - bound violations trigger abort

--- src_base/image.h
+++ src_base/image.h
@@ -49,9 +49,17 @@ class Image {
 	    if (i<0 || i>=nx || j<0 || j>=ny)
 	      {
 		cerr << "Out of range i,j,nx,ny = "<< i <<","<< j <<","<< nx <<","<< ny <<endl;
+		abort();
 	      }
 #endif
 	    return data[i+j*nx];}
+
+  /* The following routine should be Pixel ... () const.
+     It is the ImageWindow class that enforces this strange 
+     signature. We should get rid of ImageWindow and PixelIterator
+     and we could then have a reasonnable signature of this access 
+     routine.
+  */
 
       inline Pixel& operator() (const int i, const int j) const 
         {
@@ -59,6 +67,7 @@ class Image {
 	    if (i<0 || i>=nx || j<0 || j>=ny)
 	      {
 		cerr << "Out of range i,j,nx,ny = "<< i <<","<< j <<","<< nx <<","<< ny <<endl;
+		abort();
 	      }
 #endif
             return data[i+j*nx];}
@@ -196,12 +205,6 @@ class Image {
   //! put pixels above threshold to 1 and to 0 otherwise 
       void Simplify(double threshold, int above_val=1, int under_val=0);
 
-#ifndef SWIG
-  // NRL: 02/2004 : NOT IMPLEMENTED!
-  //! remove lone bad pixels ie with no bad pixels in a square of 2d+1 x 2d+1.
-  void  RemoveLonePix(Image & result, int d=1);
-#endif
-
   //! sum  pixels values
   double SumPixels(); 
   //! sum  pixels on a square centered on (x,y), side = 2*demi_size+1.
@@ -244,8 +247,10 @@ class Image {
     for (Pixel *p = begin(); p < pend; ++p) *p = F(*p);
   }
 
+  void allocate(int Nx, int Ny, int Init=1 );
+
  protected :
-      void allocate(int Nx, int Ny, int Init=1 );
+
 
       Pixel *data;
       int nx,ny;
@@ -255,74 +260,6 @@ class Image {
       friend void image_copy(Image *To, const Image *From);
 
 
-};
-
-
-class ImageWindow {
-  /* DOC This class is intended to manipulate a subimage without actually extracting it*/
-
-  public :
-  int xstart, ystart, xend, yend;
-  const Image &TheImage;
-  
-  /*! stores for later use the parameters of the subimage. */
-  ImageWindow(const Image& AnImage, int XStart, int YStart, int Width, int Height) : TheImage(AnImage)
-    {
-    xstart = max(XStart,TheImage.minx());
-    ystart = max(YStart,TheImage.miny());
-    xend   = min(XStart + Width,  TheImage.maxx()+2-TheImage.minx()); /* 1 beyond the end */
-    yend   = min(YStart + Height, TheImage.maxy()+2-TheImage.miny());/* in that case you have to add 2 and subtract minx() or miny() */
-    };
-    /*! */
-  int width() const { return xend-xstart;}
-    /*! */
-  int height() const { return yend-ystart;}
-    /*! */
-  int area() const { return width()*height();}
-    /*! */
-  friend ostream & operator << (ostream &stream, const ImageWindow &w)
-    { stream << " x y " << w.xstart << ' ' << w.ystart << " xe ye " << w.xend << ' ' << w.yend << " s " << w.area() << endl;
-      return stream;}
-  Pixel& operator()(const int x, const int y) { return TheImage(x+xstart,y+ystart);}
-};
-    
-
-
-class PixelIterator { /*! a stuff to iterate on pixels inside a subimage */
-public:
-  /*! creates the iterator */
-  PixelIterator(const ImageWindow &Window) : 
-                  lineLength(Window.TheImage.Nx()) , 
-                  jump( lineLength - Window.xend  + Window.xstart),
-                  end(&(Window.TheImage(Window.xend, Window.yend-1))),
-                  start(&(Window.TheImage(Window.xstart, Window.ystart))) 
-                        { Restart();} 
-                  
-  void Restart() {current = start; currentLineEnd = current + lineLength-jump;}
-  
-  void operator++() { if (++current==currentLineEnd)   /* postfix */ 
-                      {current+=jump; currentLineEnd += lineLength;}
-                       }
-  Pixel& operator*() const { return *current;}
-  bool BeyondEnd() const { return (current>=end);}
-  bool KeepGoing() const { return (current<end);}
-
-  /*! Here is an example on how to use it : \\ 
-  Image I(...);\\
-  ImageWindow w(I,0,0,3,3);\\
-  for (PixelIterator pi(w); pi.KeepGoing(); ++pi) cout << *pi;\\
-  */
-
-  /*! On purpose, only the prefix ++ is provided, the postfix would be much slower,
-    as for iterators of standard containers. */
-
-private :
-  Pixel *current;
-  const int  lineLength;
-  const int jump;
-  Pixel *const end;
-  Pixel *const start;
-  Pixel *currentLineEnd;
 };
 
 
