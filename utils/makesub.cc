bc45a97169f11f594b6a8f3d8509607f720310d6
Sebastien Fabbro <sfabbro@uvic.ca>
2011-07-22 15:24:04 -0700

    cosmetics

--- utils/makesub.cc
+++ utils/makesub.cc
@@ -10,7 +10,7 @@ static void usage(const char * prog)
   cerr << " usage : " << endl;
   cerr << prog << " [-o] [-S subfile] [-M]" << endl
        << "        -o : overwrite existing images (default = false)" << endl
-       << "        -S subfile : to provide a subfile name (default = \"subfile\")" 
+       << "        -S <subfile> : to provide a subfile name (default = \"subfile\")" << endl 
 #ifdef ENABLE_MC
        << "        -M : MC mode for efficiency computation" << endl;
 #else
