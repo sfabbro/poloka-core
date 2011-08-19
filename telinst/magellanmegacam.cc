9395f6852a1e5224404caccd712522c14a23f773
Sebastien Fabbro <sfabbro@uvic.ca>
2011-08-19 14:16:23 -0700

    more Magellan/Megacam routines

--- telinst/magellanmegacam.cc
+++ telinst/magellanmegacam.cc
@@ -1,6 +1,9 @@
 #ifdef VIRTUAL_INSTRUMENTS
+
 class MagellanMegacam : public VirtualInstrument { /* TYPE_SELECTOR */
+
 public:
+
   static VirtualInstrument *Acceptor(const FitsHeader &Head) { 
     if (CheckKey(Head, "TELESCOP", "clay_f5" ) && CheckKey(Head,"DETECTOR","megacam")) 
       return new MagellanMegacam;
@@ -11,13 +14,12 @@ public:
   string InstName() const { return "Megacam"; }
   string TelInstName() const { return "MagellanMegacam"; }
 
+  SIMPLE_TRANSLATOR(TOADCHIP,"EXTVER");  
   SIMPLE_TRANSLATOR(TOADMJD,"MJD");
   SIMPLE_TRANSLATOR(TOADUTIM,"UT");
   SIMPLE_TRANSLATOR(TOADPIXS,"SECPIX2");
   SIMPLE_TRANSLATOR(TOADBAND,"BFILTNIC");
   SIMPLE_TRANSLATOR(TOADFILT,"BFILTID");
-  RETURN_A_VALUE(TOADCHIP, 1);
-
 };
 #endif
 
