4b0a909360bd2db45719c013efda94b05c6fc7dd
Sebastien Fabbro <sfabbro@uvic.ca>
2011-11-04 02:28:39 -0700

    autogen.sh now uses autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,3 @@
-#!/bin/sh 
-# 
-# $Id: autogen.sh,v 1.4 2010/09/08 22:09:11 seb Exp $
-# 
-# autogen.sh 
-# 
-# run the autotools in the correct order.
-#
+#!/bin/sh -e
 
-test -d config || mkdir config
-autoheader
-aclocal -I m4
-libtoolize --copy --force
-autoconf
-automake -a --copy
+autoreconf --install --symlink
