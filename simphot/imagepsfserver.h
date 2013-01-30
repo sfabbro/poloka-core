f1c46a17a4f76b1188662a00d64c428c82811b06
Sebastien Fabbro <sfabbro@uvic.ca>
2013-01-30 13:58:27 -0800

    first round of split to core

--- simphot/imagepsfserver.h
+++
@@ -1,9 +0,0 @@
-#ifndef IMAGEPSFSERVER__H
-#define IMAGEPSFSERVER__H
-
-class ImagePSF;
-class ReducedImage;
-
-const ImagePSF* FindImagePSF(const ReducedImage *Ri);
-
-#endif /* IMAGEPSFSERVER__H */
